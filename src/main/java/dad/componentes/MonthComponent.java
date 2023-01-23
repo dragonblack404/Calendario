f685d16eb25931aced63de64155e1ce9e56fed52
Dragon Black <mdbetico.96@gmail.com>
2023-01-23 10:35:43 +0000

    first commit

---
+++ src/main/java/dad/componentes/MonthComponent.java
@@ -0,0 +1,54 @@
+package dad.componentes;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+
+public class MonthComponent extends GridPane implements Initializable {
+
+	@FXML
+	private Label labelDay;
+
+	@FXML
+	private Label nameDay1;
+
+	@FXML
+	private Label nameDay2;
+
+	@FXML
+	private Label nameDay3;
+
+	@FXML
+	private Label nameDay4;
+
+	@FXML
+	private Label nameDay5;
+
+	@FXML
+	private Label nameDay6;
+
+	@FXML
+	private Label nameDay7;
+
+	@FXML
+	private Label nameMonth;
+
+	@FXML
+	private GridPane view;
+	
+	
+	//Model
+	
+	
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
